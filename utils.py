c72f2ef8c8d1fd19694580b8c809130f47fba143
brayano <brayan@uw.edu>
2015-11-30 12:23:47 -0800

    Handles constant and linear fitting, so called.

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import cvxpy as cvx
 import scipy as scipy
 import cvxopt as cvxopt
+import matplotlib.pyplot as plt
 
 def form_D1(n):
 	D = np.zeros((n-1,n))
@@ -16,8 +17,9 @@ def form_Dk(n,k):
 		D = form_D1(n-i).dot(D)
 	return D
 
-def form_O(x,ncuts):
-	segs = np.linspace(min(x),max(x),ncuts)
+def form_O(x,ncuts,eps=0.01):
+	segs = np.linspace(min(x)-eps,max(x)+eps,ncuts)
+	# Default: bins[i-1]<= x < bins[i]
 	cats = np.digitize(x, bins=segs)
 	n = x.size
 	O = np.zeros((n, ncuts))
@@ -25,11 +27,17 @@ def form_O(x,ncuts):
 		O[i,cats[i]-1] = 1
 	return O
 
-def form_O_linear(x,ncuts):
-	segs = np.linspace(min(x),max(x),ncuts)
+def form_O_linear(x,ncuts,eps=0.01):
+	# Produce a linear interpolation knot matrix. 
+	# Unless segments are provided, we assume equal bin
+	# sizes. 
+	segs = np.linspace(min(x)-eps,max(x)+eps,ncuts)
 	cats = np.digitize(x, bins=segs)
-	t = x-segs[cats-1]
-	delta = segs[1]
+	delta = segs[1]-segs[0] # length of each bin
+	
+	# Calculate the distance between a point and the beginning
+	# of its corresponding bin. 
+	t = x-segs[cats-1] 
 	n = x.size
 	O = np.zeros((n, ncuts))
 	for i in range(n):
@@ -38,19 +46,15 @@ def form_O_linear(x,ncuts):
 		if index+1 < ncuts:
 			O[i,index+1] = (delta-t[i])/delta
 	return O
-	
 
-def linterp(x,segs,theta):
-	# x needs to be ordered: increasing.
-	cats = np.digitize(x, bins=segs)
-	t = x-segs[cats-1]
-	delta = segs[1]
-	shifttheta = np.delete(x,[0])
-	shifttheta = np.append(x,0)
-	len = x.size
-	lin_fits = np.zeros(len)
-	for i in range(len):
-		lin_fits[i] = (t[i]*theta[cats[i]-1]+(delta-t[i])*shifttheta[cats[i]-1])/delta
-	return lin_fits
-	
-	
+def Omatrix(x,ncuts,constant=True,eps=0.01):
+	# Currently, Omatrix outputs either constant knot matrix
+	# or linearly interpolated knot matrix. Eventually,
+	# "constant" will be deprecated for "degree" option.
+	# degree=0; constant. degree=1; linear. degree=2; quadratic. etc. 
+	if constant == True:
+		O = form_O(x,ncuts,eps=eps)
+	if constant != True:
+		O = form_O_linear(x,ncuts,eps=eps)
+	return O
+
