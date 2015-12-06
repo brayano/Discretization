c66a85d9791f6643ef23cd075541b59b0df11eb9
brayano <brayan@uw.edu>
2015-12-05 19:06:53 -0800

    Added discplot: given disc object, plot data against fitted values.

--- plotdisc.py
+++ plotdisc.py
@@ -4,7 +4,7 @@ import scipy as scipy
 import discsolvers as ds 
 	
 
-def dtplot(x,y,vtune,k,ncuts,truefxn,constant=True,eps=0.01,lnorm=1,xlab='X',ylab='Y'):
+def dtplot(x,y,vtune,k,ncuts,truefxn,segs=None,constant=True,eps=0.01,lnorm=1,xlab='X',ylab='Y'):
 	# In this function, we plot discrete approximations to 
 	# functional minimization problems knowing the truth. 
 	# This function is for personal use only.
@@ -23,12 +23,13 @@ def dtplot(x,y,vtune,k,ncuts,truefxn,constant=True,eps=0.01,lnorm=1,xlab='X',yla
 	
 	# Create the coefficients
 	for i in range(tsize):
-		fits.append( ds.disc_one(x,y,ncuts, k, tune = vtune[i],eps=eps,constant=constant,lnorm=lnorm) )
+		fits.append( ds.disc_one(x,y,ncuts, k, segs=segs,tune = vtune[i],eps=eps,constant=constant,lnorm=lnorm) )
 	
 	order = np.argsort(x)
 	xs = np.array(x)[order]
 	ys = np.array(y)[order]
-	segs = np.linspace(min(x)-eps,max(x)+eps,ncuts)
+	if segs==None:
+		segs = np.linspace(min(x)-eps,max(x)+eps,ncuts)
 	
 	plt.plot(s,r,'k-')
 	plt.scatter(xs,ys,c='k',alpha=0.1,s=10)
@@ -45,6 +46,35 @@ def dtplot(x,y,vtune,k,ncuts,truefxn,constant=True,eps=0.01,lnorm=1,xlab='X',yla
 	plt.ylabel(ylab)
 	plt.show()
 
+
+def segcenter(segs):
+	seglens = np.ediff1d(segs)
+	seglens = np.append(seglens,seglens[-1]+.01)
+	segmidlens = seglens/2
+	delta = segs+segmidlens
+	return delta
+
+
+def discplot(x,y,ncuts,segs=None,xlab='X',ylab='Y'):
+	# In the disc object (disco), x=object[2], y=object[3],segs = object[4],constant=object[5]
+	# eps = disco[6]
+	order = np.argsort(disco[2])
+	xs = np.array(disco[2])[order]
+	ys = np.array(disco[3])[order]
+	plt.scatter(xs,ys,c='k',alpha=0.1,s=10)
+	plt.vlines(disco[4],min(disco[3])-disco[6],max(disco[3])+disco[6],alpha=0.5)
+	if disco[5] == True:
+		delta = segcenter(disco[4]
+		plt.plot(disco[0]+delta,disco[1])
+	if disco[5] == False:
+		plt.plot(disco[0][order],disco[1][order])
+	plt.xlabel(xlab)
+	plt.xlabel(ylab)
+	plt.show()
+
+
+
+
 # Testing 	
 #np.random.seed([117])
 #x = np.random.normal(0,2,200)
@@ -54,3 +84,4 @@ def dtplot(x,y,vtune,k,ncuts,truefxn,constant=True,eps=0.01,lnorm=1,xlab='X',yla
 #fig = plt.figure(figsize=(6, 4))
 #dtplot(x,y,vtune=[1,10,30],ncuts=100,truefxn=wsin,k=2,constant=True,lnorm=1)
 #fig.savefig("test.png")
+
